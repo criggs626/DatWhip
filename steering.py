c5dcb5f85cc522c2f302ac76ce5bf93fbc2323c7
Caleb Riggs <criggs2892@flpoly.org>
2018-02-14 16:46:27 -0500

    Steering in circle

--- steering.py
+++ steering.py
@@ -1,14 +1,14 @@
 import rospy as rp
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
-def driveTest():	
+def driveTest():
 	rp.init_node("pleaseWork",anonymous = False)
 	pub=rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation",AckermannDriveStamped,queue_size=10)
 	rate=rp.Rate(60)
 	drive_msg_stamped = AckermannDriveStamped()
 	drive_msg = AckermannDrive()
-       	drive_msg.speed = 2.0
-        drive_msg.steering_angle = 0.2
+       	drive_msg.speed = 0.8
+        drive_msg.steering_angle = 1.0
         drive_msg.acceleration = 0
         drive_msg.jerk = 0
         drive_msg.steering_angle_velocity = 0
@@ -22,4 +22,3 @@ if __name__ =="__main__":
 		driveTest()
 	except rp.ROSInterruptException:
 		pass
-
